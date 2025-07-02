43e30b5d9ddbbe24e552f6d1cd573f22ad6b1a80
dfmika08 <150296435+dfmika08@users.noreply.github.com>
2025-07-02 11:54:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,4 +45,7 @@ def encrypt_pdf():
         return f"Encryption failed: {str(e)}", 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
